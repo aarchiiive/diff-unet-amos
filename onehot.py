fb80eb578158dedaaee0ac9f2920d466ffa42a9d
aarchiiive <wychrodjxz691@gmail.com>
2023-10-02 01:48:32 +0000

    deleted

--- onehot.py
+++
@@ -1,7 +0,0 @@
-import torch
-from torch.nn.functional import one_hot
-
-num_classes = 6
-labels = torch.randint(0, 5, (4, 1, 512 ,512))
-labels = torch.cat([labels == i for i in range(num_classes)], dim=1)
-print(labels.shape)
