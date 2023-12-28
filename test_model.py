ecf67a01ea8a9a80f88c70c417c1485ee5d4a36b
aarchiiive <wychrodjxz691@gmail.com>
2023-12-28 16:24:42 +0000

    Add DistanceLabelSmothing class to test_model.py

--- test_model.py
+++ test_model.py
@@ -193,7 +193,42 @@ def test_dice():
     print(dices)
     print(f"Time: {time.time() - t0:.4f}s")
     
+class DistanceLabelSmothing(nn.Module):
+    def __init__(
+        self, 
+        num_classes: int,
+        alpha: float = 0.1,
+        beta: float = 0.1,
+        epsilon: float = 1e-6,
+    ):
+        super().__init__()
+        self.num_classes = num_classes
+        self.epsilon = epsilon
+        
+        self.alpha = nn.ParameterList([nn.Parameter(torch.tensor([alpha])) for _ in range(num_classes)])
+        self.beta = nn.ParameterList([nn.Parameter(torch.tensor([beta])) for _ in range(num_classes)])
+        
+    def forward(self, labels: torch.Tensor, distances: torch.Tensor) -> torch.Tensor:
+        org = labels
+        for i in range(self.num_classes):
+            labels[:, i] = self.rational(distances[:, i], i) # [0, 1, 0, 0, 0, 0] [0, 0, 1, 0, 0, 0]
+            
+        labels = torch.abs(org - labels)
+        
+        return labels
+    
+    def rational(self, x: torch.Tensor, i: int) -> torch.Tensor:
+        return 1 / (self.beta[i] * x + self.epsilon) * self.alpha[i] # 1 / x * alpha = 0.1
+    
+    def damped_sine(self, x: torch.Tensor) -> torch.Tensor:
+        return torch.sin(x) / x
+
+
 if __name__ == "__main__":
-    test_dice()
+    x = torch.randn((1, 13, 96, 96, 96))
+    d = torch.randn((1, 13, 96, 96, 96))
+    l = DistanceLabelSmothing(13)
+    print(l(x, d).shape)
+    # test_dice()
     # test_dataset()
     # test_model()
