d6ea096f13e1c67e9ae67e37e7028e42ac3ada3c
aarchiiive <wychrodjxz691@gmail.com>
2023-12-28 16:24:54 +0000

    Add compute_distance method to CacheDataset

--- dataset/cache_dataset.py
+++ dataset/cache_dataset.py
@@ -6,6 +6,7 @@ import collections.abc
 
 from copy import deepcopy
 from collections.abc import Callable, Sequence
+from typing import Dict
 from multiprocessing.managers import ListProxy
 
 import torch
@@ -65,10 +66,42 @@ class LabelSmoothingCacheDataset(CacheDataset):
             hash_func=hash_func,
             runtime_cache=runtime_cache,
         )
+        
+    def compute_distance(self, labels: torch.Tensor) -> Dict[str, torch.Tensor]:
+        org = labels
+        labels = F.one_hot(labels.long(), num_classes=self.num_classes).permute(0, 4, 1, 2, 3).float()
+        B, C, W, H, D = labels.shape
+        
+        # Pre-compute all indices
+        indices = torch.stack(torch.meshgrid(
+            torch.arange(W),
+            torch.arange(H),
+            torch.arange(D),
+            indexing='ij'
+        ), dim=-1).float().to(labels.device)
+        
+        centroids = torch.zeros(self.num_classes, 3, device=labels.device)
+        
+        for i in range(self.num_classes):
+            class_mask = labels[:, i, :, :, :]
+            class_mask = class_mask.squeeze(0).bool()
+            
+            masked_indices = indices[class_mask]
+            if masked_indices.numel() > 0:
+                centroid = masked_indices.mean(dim=0)
+                centroids[i] = centroid
+                
+        centroids = centroids[:, None, None, None, None, :]
+        
+        distances = torch.norm(indices[None, None, :, :, :, :] - centroids, dim=-1).squeeze(1)
+        distances_dict = {f"distance_{i}": distances[i, :, :, :].unsqueeze(0) for i in range(self.num_classes)}
+        
+        return distances_dict
     
     def label_smoothing(self, labels: torch.Tensor) -> torch.Tensor:
+        org = labels
         labels = F.one_hot(labels.long(), num_classes=self.num_classes).permute(0, 4, 1, 2, 3).float()
-        org = labels.squeeze(0)
+        # org = labels.squeeze(0)
         B, C, W, H, D = labels.shape
         
         # Pre-compute all indices
@@ -103,13 +136,14 @@ class LabelSmoothingCacheDataset(CacheDataset):
         centroids = centroids[:, None, None, None, None, :]
 
         # Calculate distances with correct broadcasting
-        distances = torch.norm(indices[None, None, :, :, :, :] - centroids, dim=-1)
+        distances = torch.norm(indices[None, None, :, :, :, :] - centroids, dim=-1).squeeze(1).unsqueeze(0)
         
         # labels = 1 / (distances.squeeze(1) + self.epsilon) * self.smoothing_alpha
-        labels = self.damped_sine_wave(distances.squeeze(1)) * self.smoothing_alpha # wandb : diff-swin-unetr-btcv-10
-        labels = torch.abs(org - labels)
+        # labels = self.damped_sine_wave(distances.squeeze(1)) * self.smoothing_alpha # wandb : diff-swin-unetr-btcv-10
         
-        return labels
+        # labels = torch.abs(org - labels)
+        
+        return distances
     
     def damped_sine_wave(self, x: torch.Tensor, lambda_decay=0.05, omega=0.1, phi=0) -> torch.Tensor:
         signal = torch.exp(-lambda_decay * x) * torch.sin(omega * x + phi)
@@ -124,65 +158,33 @@ class LabelSmoothingCacheDataset(CacheDataset):
         item = self.image_loader(item)
         
         if self.smoothing_type == "distance":
-            item['label'] = self.label_smoothing(item['label'])
+            for k, v in self.compute_distance(item['label']).items():
+                item[k] = v
+            
+        # print(self.data[idx], item['image'].shape, item['label'].shape, item['distance_0'].shape)
             
         first_random = self.transform.get_index_of_first(
             lambda t: isinstance(t, RandomizableTrait) or not isinstance(t, Transform)
         )
         item = self.transform(item, end=first_random, threading=True)
-
+        
+        print("After transform:", item['image'].shape, item['label'].shape, item['distance_0'].shape, item['distance_1'].shape)
+        
         if self.as_contiguous:
             item = convert_to_contiguous(item, memory_format=torch.contiguous_format)
         return item
     
-    
-class ValidCacheDataset(CacheDataset):
-    def __init__(
-        self,
-        data: Sequence,
-        transform: Sequence[Callable] | Callable | None = None,
-        cache_num: int = sys.maxsize,
-        cache_rate: float = 1.0,
-        num_workers: int | None = 1,
-        progress: bool = True,
-        copy_cache: bool = True,
-        as_contiguous: bool = True,
-        hash_as_key: bool = False,
-        hash_func: Callable[..., bytes] = pickle_hashing,
-        runtime_cache: bool | str | list | ListProxy = False,
-    ) -> None:
-        self.image_loader = transforms.Compose([
-            LoadImaged(keys=["image", "label"], ensure_channel_first=True),
-        ])
-        
-        super().__init__(
-            data=data,
-            transform=transform,
-            cache_num=cache_num,
-            cache_rate=cache_rate,
-            num_workers=num_workers,
-            progress=progress,
-            copy_cache=copy_cache,
-            as_contiguous=as_contiguous,
-            hash_as_key=hash_as_key,
-            hash_func=hash_func,
-            runtime_cache=runtime_cache,
-        )
-    
-    def _load_cache_item(self, idx: int):
+    def __getitem__(self, index: int | slice | Sequence[int]):
         """
-        Args:
-            idx: the index of the input data sequence.
+        Returns a `Subset` if `index` is a slice or Sequence, a data item otherwise.
         """
-        item = self.data[idx]
-        item = self.image_loader(item)
-        item['raw_label'] = item['label']
-        
-        first_random = self.transform.get_index_of_first(
-            lambda t: isinstance(t, RandomizableTrait) or not isinstance(t, Transform)
-        )
-        item = self.transform(item, end=first_random, threading=True)
-
-        if self.as_contiguous:
-            item = convert_to_contiguous(item, memory_format=torch.contiguous_format)
-        return item
+        if isinstance(index, slice):
+            # dataset[:42]
+            start, stop, step = index.indices(len(self))
+            indices = range(start, stop, step)
+            return Subset(dataset=self, indices=indices)
+        if isinstance(index, collections.abc.Sequence):
+            # dataset[[1, 3, 4]]
+            return Subset(dataset=self, indices=index)
+        return self._transform(index)
+    
